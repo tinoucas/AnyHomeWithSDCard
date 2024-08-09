0a208f8bb6c8c8398c4ed0e4a8972ed06e617ab9
Tinou <tinoCucaAs@gmTail.com (take the cat out)>
2024-08-09 20:36:18 +0200

    Added: requirement for sd-card mounted before launching chosen 'launcher' app.

--- app/src/main/java/com/draco/anyhome/views/LauncherActivity.kt
+++ app/src/main/java/com/draco/anyhome/views/LauncherActivity.kt
@@ -1,16 +1,31 @@
 package com.draco.anyhome.views
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.os.Bundle
+import android.os.Environment
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.draco.anyhome.viewmodels.LauncherActivityViewModel
 
+
 class LauncherActivity : AppCompatActivity() {
     private val viewModel: LauncherActivityViewModel by viewModels()
 
+    private fun startActivityIfStorageMounted() {
+        val state = Environment.getExternalStorageState()
+
+        if (Environment.MEDIA_MOUNTED == state && viewModel.isHomeAppSet()) {
+            viewModel.homeAppIntent?.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+            startActivity(viewModel.homeAppIntent)
+            overridePendingTransition(0, 0)
+        }
+    }
+
     override fun onResume() {
         super.onResume()
-
         if (!viewModel.isHomeAppSet()) {
             val intent = Intent(this, SelectActivity::class.java)
             startActivity(intent)
@@ -18,8 +33,16 @@ class LauncherActivity : AppCompatActivity() {
 
             return
         }
-
-        startActivity(viewModel.homeAppIntent)
-        overridePendingTransition(0, 0)
+        val externalStorageStateReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                Thread.sleep(1000)
+                startActivityIfStorageMounted()
+            }
+        }
+        val filter = IntentFilter()
+        filter.addAction(Intent.ACTION_MEDIA_MOUNTED)
+        filter.addDataScheme("file");
+        registerReceiver(externalStorageStateReceiver, filter)
+        startActivityIfStorageMounted()
     }
 }
